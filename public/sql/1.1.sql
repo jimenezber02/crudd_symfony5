b7a5e968d055a5e5811486096d69d4ff5f60ec72
jimenezber02 <88626108+jimenezber02@users.noreply.github.com>
2022-03-17 21:18:49 -0500

    as

--- public/sql/1.1.sql
+++ public/sql/1.1.sql
@@ -1,10 +1,9 @@
 CREATE TABLE `user` ( 
-    `id` INT NOT NULL , 
+    `id` INT NOT NULL AUTO_INCREMENT, 
     `email` VARCHAR(180) NOT NULL , 
     `roles` JSON NOT NULL , 
-    `password` VARCHAR(200) NOT NULL ),
-    PRIMARY KEY (`id`)) 
-    ENGINE = InnoDB;
+    `password` VARCHAR(200) NOT NULL,
+    PRIMARY KEY (`id`)) ;
 
 CREATE TABLE `datos`(
     `id` INT NOT NULL AUTO_INCREMENT ,
@@ -13,4 +12,4 @@ CREATE TABLE `datos`(
     `sexo` VARCHAR(2) NOT NULL ,
     `imagen` VARCHAR(200) NOT NULL ,
     `activo` INT NOT NULL ,
-    KEY (`id`)) ENGINE = InnoDB;
+    PRIMARY KEY (`id`));
